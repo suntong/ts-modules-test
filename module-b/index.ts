b261fb0c99af7026cb8c31d8ceb39089f3c7875d
Tong Sun <suntong@cpan.org>
2019-01-01 16:07:57 -0500

    - [+] add & use myfunc()

--- module-b/index.ts
+++ module-b/index.ts
@@ -1,4 +1,7 @@
 import { add, multiply, divide } from "module-a";
 
-const result = divide(multiply(add(1, 2), 2), 2);
+export const myfunc = (a: number, b: number) =>
+       divide(multiply(add(a, b), 2), 2);
+
+const result = myfunc(1, 2);
 console.log(result);
